5978f4376d817480beacade015ec2dcf48405b2f
muniz94 <muniz91@hotmail.com>
2023-12-23 13:02:58 -0300

    Salvei os dados do calendario CRUD no localStorage

--- Calendario__CRUD/script.js
+++ Calendario__CRUD/script.js
@@ -1,5 +1,44 @@
 const formulario = document.querySelector('.formulario');
+const botao = document.querySelector('.formulario__botao');
+const campoNome = document.querySelector('.formulario__blocoDeCampos__nome');
+const campoData = document.querySelector('.formulario__blocoDeCampos__data');
+const tabela = document.querySelector('.tabela');
+
+const pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
+
+function criarTabela(item) {
+  const li = document.createElement('li');
+  li.textContent = 'Nome: ' + item.nome + ' ------ Data de Nascimento: ' + item.data_de_nascimento;
+
+  return li;
+}
 
 formulario.addEventListener('submit', (evento) => {
   evento.preventDefault();
-})
+
+  const pessoa = {
+    nome: campoNome.value,
+    data_de_nascimento: campoData.value,
+  }
+  pessoas.push(pessoa);
+  const elemento = criarTabela(pessoa);
+  tabela.append(elemento);
+
+  campoNome.value = "";
+  campoData.value = "";
+
+  localStorage.setItem('pessoas', JSON.stringify(pessoas));
+  
+});
+
+pessoas.forEach(item => {
+  const elemento = criarTabela(item);
+  tabela.append(elemento);
+});
+
+ 
+
+
+
+
+
