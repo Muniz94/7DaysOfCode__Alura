d134bb7848aef149c602f5c9d1fd453dcbd29fda
muniz94 <muniz91@hotmail.com>
2023-12-25 21:07:57 -0300

    Agora pode-se remover itens da lista

--- JS - Op. Booleanas/Lista__Supermercado/script.js
+++ JS - Op. Booleanas/Lista__Supermercado/script.js
@@ -1,6 +1,7 @@
 let resposta;
 let item;
 let categoria;
+let deletar;
 const lista = document.querySelector('.lista');
 
 const categorias = {
@@ -9,6 +10,8 @@ const categorias = {
   doces: [],
   laticinios: [], 
 }
+
+
 
 resposta = confirm("Você deseja adicionar itens a sua lista de supermercado?");
 while (resposta != false) {
@@ -26,10 +29,29 @@ while (resposta != false) {
     categorias.laticinios.push(item);
   }
 
+  
+  if (categorias.congelados.length != 0 || categorias.frutas.length != 0 || categorias.doces.length != 0 || categorias.laticinios.length != 0) {
+    deletar = confirm("Você quer deletar algum item da lista?");
+    if (deletar === true) {
+      alert(`Itens da lista de compras: ${categorias.doces} || ${categorias.congelados} || ${categorias.frutas} || ${categorias.laticinios}`)
+      deletar = prompt("Qual item você quer deletar?");
+      if (categorias.doces.includes(deletar) || categorias.congelados.includes(deletar) || categorias.frutas.includes(deletar) || categorias.laticinios.includes(deletar)) {
+        categorias.doces = categorias.doces.filter(item => item != deletar);
+        categorias.congelados = categorias.congelados.filter(item => item != deletar);
+        categorias.frutas = categorias.frutas.filter(item => item != deletar);
+        categorias.laticinios = categorias.laticinios.filter(item => item != deletar);
+      } else {
+        if (deletar != null) {
+          alert("Não foi possível encontrar seu item na lista");
+        }
+      }
+    }
+  }
+  
   resposta = confirm("Você deseja adicionar itens a sua lista de supermercado?");
 }
 
-const paragrafo = document.createElement('p');
+  const paragrafo = document.createElement('p');
 
   paragrafo.innerHTML = `Lista de Compras: <br> Congelados: ${categorias.congelados} <br> Frutas: ${categorias.frutas} 
   <br> Doces: ${categorias.doces} <br> Laticínios: ${categorias.laticinios} `;
