1923959170e4bfa3d94b2f57c4f6099912a733c0
muniz94 <muniz91@hotmail.com>
2024-01-08 21:51:37 -0300

    Renderizar a lista de filmes dinamicamente com o DOM

---
+++ The_Movie_DataBase/script.js
@@ -0,0 +1,74 @@
+const listaFilmes = document.querySelector('.filmes');
+
+const filmes = [
+  {
+    image: 'imagens/Vingadores.svg',
+    title: 'Avengers Endgame (2019)',
+    rating: 9.2,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    isFavorited: false
+  },
+  {
+    image: 'imagens/Batman.svg',
+    title: 'Batman (2022)',
+    rating: 9.2,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    isFavorited: false
+  },
+  {
+    image: 'imagens/Doutor Estranho.svg',
+    title: 'Doctor Strange in the Multiverse of Madness',
+    rating: 9.2,
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    isFavorited: true
+  },
+]
+
+window.onload = function() {
+  filmes.forEach(filme => adicionarFilme(filme))
+}
+
+function adicionarFilme(filme) {
+
+  const li = document.createElement('li');
+  li.classList.add('filmes__card');
+  listaFilmes.append(li);
+
+  const imagem = document.createElement('img');
+  imagem.classList.add('filmes__card__imagem');
+  imagem.setAttribute('src', filme.image);
+
+  li.append(imagem);
+
+  const divInfo = document.createElement('div');
+  divInfo.classList.add('filmes__card__div');
+  
+
+  const titulo = document.createElement('h2');
+  titulo.classList.add('filmes__card__div__titulo');
+  titulo.textContent = `${filme.title}`
+  divInfo.append(titulo)
+  const divFavoritos = document.createElement('div');
+  divFavoritos.classList.add('filmes__card__div__favoritos');
+  const curtir = document.createElement('img');
+  curtir.src = 'imagens/Star.svg'
+  divFavoritos.append(curtir);
+  const txtCurtir = document.createElement('span');
+  txtCurtir.textContent = filme.rating;
+  divFavoritos.append(txtCurtir)
+  divInfo.append(divFavoritos);
+  const curtir1 = document.createElement('img');
+  curtir1.src = filme.isFavorited ? 'imagens/Coracao_preenchido.svg' : 'imagens/Coracao.svg';
+  divFavoritos.append(curtir1);
+  const txtCurtir1 = document.createElement('span');
+  txtCurtir1.textContent = 'Favoritar';
+  divFavoritos.append(txtCurtir1)
+
+  li.append(divInfo);
+
+  const descricao = document.createElement('p');
+  descricao.classList.add('filmes__card__descricao');
+  descricao.textContent = `${filme.description}`;
+
+  li.append(descricao);
+}
